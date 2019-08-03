42423fb7020edba1741141348000202c393b8df7
Mocha <mocha2007andcoco@gmail.com>
2019-08-03 19:50:07 -0400

    halfway done with line code...

--- mocha_ruler/ui/world_explorer.lua
+++ mocha_ruler/ui/world_explorer.lua
@@ -4,6 +4,7 @@ function myMochaFunctionOWO()
     leftCoords = {0, 0}
     rightCoords = {0, 0}
 	myMochaText = Spawn(lgo.textField, ui)
+	color_blue = Color32(0, 0, 255, 255)
 	color_red = Color32(255, 0, 0, 255)
 end
 
@@ -23,8 +24,12 @@ function on_tile_right_clicked()
 	-- color/log
 	world.tileCalaDebugColors[leftTileID] = color_red
 	world.tileCalaDebugColors[rightTileID] = color_red
+
+	for _, lineTileID in pairs(line_between(leftTileID, rightTileID)) do
+		world.tileCalaDebugColors[lineTileID] = color_red
+	end
 	world.UpdateMapMode(mapModes.DEBUG_COLOR_2)
-	log(currentDistance)
+	-- log(currentDistance)
 end
 
 local foo = on_world_explorer_loaded
--- mocha_syntactic_sugar/mocha.lua
+++ mocha_syntactic_sugar/mocha.lua
@@ -20,6 +20,22 @@ function coord_dist(coords1, coords2)
 	return R * c
 end
 
+function line_between(tile1ID, tile2ID)
+	local line_array = {tile1ID}
+	while line_array[#line_array] != tile2ID and line_array[#line_array] != line_array[#line_array - 1] do
+		log("lb3 "..tostring(line_array[#line_array]))
+		table.insert(line_array, tile_neighbor_closest_to(line_array[#line_array], tile2ID));
+	end
+	log("lb4")
+	return line_array
+end
+
+function neighbors(tileID)
+	-- UL UR R DR DL L
+	-- todo; broken
+	return {tileID+355, tileID+1, tileID-353, tileID-354, tileID-1, tileID+354}
+end
+
 function tile_coords(tileID)
 	local latitude = math.pi/2 - world.tileColatitudes[tileID]
 	local longitude = -world.tileMinusLongitudes[tileID]
@@ -32,4 +48,48 @@ function tile_dist(tile1ID, tile2ID)
 	return coord_dist(coords1, coords2)
 end
 
+function tile_neighbor_closest_to(tile1ID, tile2ID)
+	-- returns the ID of the neighbor of tile1 which is CLOSEST to tile2
+	-- world.tileNeighbors[tile1ID]
+	local tile1_neighbors = neighbors(tile1ID)
+	local best_id = tile1ID
+	local best_dist = tile_dist(tile1ID, tile2ID)
+	-- log(tile1ID)
+	-- log(type(tile1_neighbors))
+	log('OwOcean cUwUents')
+	log(table.string(tile1_neighbors))
+	for _, t1n in pairs(tile1_neighbors) do
+		local new_dist = tile_dist(t1n, tile2ID)
+		log(tostring(new_dist)..' ? '..tostring(best_dist))
+		if new_dist < best_dist then
+			best_id = t1n
+			best_dist = new_dist
+		end
+	end
+	return best_id
+end
+
 -- men
+
+-- https://stackoverflow.com/a/24823383/2579798
+function table.slice(tbl, first, last, step)
+	local sliced = {}
+	for i = first or 1, last or #tbl, step or 1 do
+		sliced[#sliced+1] = tbl[i]
+	end
+	return sliced
+end
+
+-- https://stackoverflow.com/a/27028488/2579798
+function table.string(o)
+	if type(o) == 'table' then
+		local s = '{ '
+		for k,v in pairs(o) do
+			if type(k) ~= 'number' then k = '"'..k..'"' end
+			s = s .. '['..k..'] = ' .. table.string(v) .. ','
+		end
+		return s .. '} '
+	else
+		return tostring(o)
+	end
+end
