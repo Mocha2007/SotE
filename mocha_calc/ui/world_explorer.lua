f1452386687c84e323778ad57027449120b23ecb
Mocha <mocha2007andcoco@gmail.com>
2019-08-04 09:56:53 -0400

    456

--- mocha_calc/ui/world_explorer.lua
+++ mocha_calc/ui/world_explorer.lua
@@ -1,8 +1,8 @@
-function myMochaFunctionOWO()
+function myMochaCalcOWO()
 	mem = {0}
 	calcText = Spawn(lgo.textField, ui)
 	Reorient_TopLeft(calcText)
-	-- Move(-155, -140, calcText)
+	Move(0, -20, calcText)
 	Resize(100, 20, calcText)
 	refreshCalc()
 
@@ -13,7 +13,7 @@ function myMochaFunctionOWO()
 		refreshCalc()
 	]], button_negate)
 	Reorient_TopLeft(button_negate)
-	Move(0, 20, button_negate)
+	Move(0, -40, button_negate)
 	Resize(20, 20, button_negate)
 	SetTextField("~", button_negate)
 	AddTooltip([[
@@ -29,7 +29,7 @@ function myMochaFunctionOWO()
 		refreshCalc()
 	]], button_sqrt)
 	Reorient_TopLeft(button_sqrt)
-	Move(20, 20, button_sqrt)
+	Move(20, -40, button_sqrt)
 	Resize(20, 20, button_sqrt)
 	SetTextField("R", button_sqrt)
 	AddTooltip([[
@@ -45,7 +45,7 @@ function myMochaFunctionOWO()
 		refreshCalc()
 	]], button_square)
 	Reorient_TopLeft(button_square)
-	Move(40, 20, button_square)
+	Move(40, -40, button_square)
 	Resize(20, 20, button_square)
 	SetTextField("S", button_square)
 	AddTooltip([[
@@ -62,7 +62,7 @@ function myMochaFunctionOWO()
 		refreshCalc()
 	]], button_pow)
 	Reorient_TopLeft(button_pow)
-	Move(60, 20, button_pow)
+	Move(60, -40, button_pow)
 	Resize(20, 20, button_pow)
 	SetTextField("^", button_pow)
 	AddTooltip([[
@@ -78,7 +78,7 @@ function myMochaFunctionOWO()
 		refreshCalc()
 	]], button_log)
 	Reorient_TopLeft(button_log)
-	Move(80, 20, button_log)
+	Move(80, -40, button_log)
 	Resize(20, 20, button_log)
 	SetTextField("L", button_log)
 	AddTooltip([[
@@ -91,7 +91,7 @@ function myMochaFunctionOWO()
 	button_7 = Spawn(lgo.textButton, ui)
 	SetCode([[keypadNum(7)]], button_7)
 	Reorient_TopLeft(button_7)
-	Move(0, 40, button_7)
+	Move(0, -60, button_7)
 	Resize(20, 20, button_7)
 	SetTextField("7", button_7)
 	AddTooltip([[
@@ -104,7 +104,7 @@ function myMochaFunctionOWO()
 	button_8 = Spawn(lgo.textButton, ui)
 	SetCode([[keypadNum(8)]], button_8)
 	Reorient_TopLeft(button_8)
-	Move(20, 40, button_8)
+	Move(20, -60, button_8)
 	Resize(20, 20, button_8)
 	SetTextField("8", button_8)
 	AddTooltip([[
@@ -117,7 +117,7 @@ function myMochaFunctionOWO()
 	button_9 = Spawn(lgo.textButton, ui)
 	SetCode([[keypadNum(9)]], button_9)
 	Reorient_TopLeft(button_9)
-	Move(40, 40, button_9)
+	Move(40, -60, button_9)
 	Resize(20, 20, button_9)
 	SetTextField("9", button_9)
 	AddTooltip([[
@@ -134,7 +134,7 @@ function myMochaFunctionOWO()
 		refreshCalc()
 	]], button_div)
 	Reorient_TopLeft(button_div)
-	Move(60, 40, button_div)
+	Move(60, -60, button_div)
 	Resize(20, 20, button_div)
 	SetTextField("/", button_div)
 	AddTooltip([[
@@ -150,7 +150,7 @@ function myMochaFunctionOWO()
 		refreshCalc()
 	]], button_clear)
 	Reorient_TopLeft(button_clear)
-	Move(80, 40, button_clear)
+	Move(80, -60, button_clear)
 	Resize(20, 40, button_clear)
 	SetTextField("CLS", button_clear)
 	AddTooltip([[
@@ -159,6 +159,45 @@ function myMochaFunctionOWO()
 		end
 	]], button_clear)
 
+	-- 4
+	button_4 = Spawn(lgo.textButton, ui)
+	SetCode([[keypadNum(4)]], button_4)
+	Reorient_TopLeft(button_4)
+	Move(0, -80, button_4)
+	Resize(20, 20, button_4)
+	SetTextField("4", button_4)
+	AddTooltip([[
+		function get_tooltip()
+			return "Four"
+		end
+	]], button_4)
+
+	-- 5
+	button_5 = Spawn(lgo.textButton, ui)
+	SetCode([[keypadNum(5)]], button_5)
+	Reorient_TopLeft(button_5)
+	Move(20, -80, button_5)
+	Resize(20, 20, button_5)
+	SetTextField("5", button_5)
+	AddTooltip([[
+		function get_tooltip()
+			return "Five"
+		end
+	]], button_5)
+
+	-- 6
+	button_6 = Spawn(lgo.textButton, ui)
+	SetCode([[keypadNum(6)]], button_6)
+	Reorient_TopLeft(button_6)
+	Move(40, -80, button_6)
+	Resize(20, 20, button_6)
+	SetTextField("6", button_6)
+	AddTooltip([[
+		function get_tooltip()
+			return "Six"
+		end
+	]], button_6)
+
 	-- Enter
 	button_enter = Spawn(lgo.textButton, ui)
 	SetCode([[
@@ -166,9 +205,9 @@ function myMochaFunctionOWO()
 		refreshCalc()
 	]], button_enter)
 	Reorient_TopLeft(button_enter)
-	Move(80, 80, button_enter)
+	Move(80, -100, button_enter)
 	Resize(20, 40, button_enter)
-	SetTextField("Enter", button_enter)
+	SetTextField("↵", button_enter)
 	AddTooltip([[
 		function get_tooltip()
 			return "Enter"
@@ -183,7 +222,7 @@ function myMochaFunctionOWO()
 		refreshCalc()
 	]], button_plus)
 	Reorient_TopLeft(button_plus)
-	Move(60, 100, button_plus)
+	Move(60, -120, button_plus)
 	Resize(20, 20, button_plus)
 	SetTextField("+", button_plus)
 	AddTooltip([[
@@ -203,4 +242,4 @@ function refreshCalc()
 end
 
 local foo = on_world_explorer_loaded
-on_world_explorer_loaded = function() foo() myMochaFunctionOWO() end
+on_world_explorer_loaded = function() foo() myMochaCalcOWO() end
